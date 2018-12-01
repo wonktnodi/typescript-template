a8b9eb19690c1fa6eaf83b1872732011cf503690
wonktnodi <wonktnodiq@gmail.com>
2018-12-01 18:06:32 +0800

    redux devtools

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,15 +1,27 @@
 import { createEpicMiddleware } from "redux-observable";
 import { applyMiddleware, createStore } from "redux";
 import logger from "redux-logger";
+import { composeWithDevTools } from "redux-devtools-extension/developmentOnly";
+
 import { rootReudcer } from "../reducers";
 import { rootEpic } from "../epics";
+
+const composeEnhancers = composeWithDevTools({
+  // Specify here name, actionsBlacklist, actionsCreators and other options
+});
+
+// console.log("env: ", process.env.NODE_ENV);
+// const composeEnhancers =
+//   /*process.env.NODE_ENV === "development" &&*/
+//   ((window as any) && (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+//   compose;
 
 const epicMiddleware = createEpicMiddleware();
 
 export default function configureStore() {
   const store = createStore(
     rootReudcer,
-    applyMiddleware(epicMiddleware, logger)
+    composeEnhancers(applyMiddleware(epicMiddleware, logger))
   );
 
   epicMiddleware.run(rootEpic);
